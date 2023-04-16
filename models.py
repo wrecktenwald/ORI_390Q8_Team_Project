951e6e01c03c2c22023afc9456517c0bbad7d58b
wrecktenwald <wrecktenwald@utexas.edu>
2023-04-16 16:25:52 -0500

    Adding class implementation of both models currently running

---
+++ models.py
@@ -0,0 +1,402 @@
+"""
+Implement general models for use in project
+"""
+
+import numpy as np
+import pandas as pd
+from pyomo.opt import SolverFactory
+import pyomo.kernel as pmo
+import pyomo.environ as pyo
+import time
+
+from utilities import elapsed_time_display
+
+
+class model:
+    """
+    Parent model class
+
+    Attributes
+    __________
+    periods : int
+        Number of periods to run model for, must match length of period indexed arrays
+    D : numpy.array
+        Maximum discharge rate indexed by period as 1-D numpy.array
+    C : numpy.array
+        Maximum charge rate indexed by period as 1-D numpy.array
+    S : numpy.array
+        Storage capacity indexed by period as 1-D numpy.array
+    p : numpy.array
+        Energy price indexed by period as 1-D numpy.array 
+    r : float
+        Discount rate
+    R : numpy.array or float
+        Round trip efficiency matrix time dependent on charge (i), discharge (j) periods or non time dependent
+    vc : float
+        Variable charge cost
+    vd : float
+        Variable discharge cost
+    solver : str
+        Solver to use in SolverFactory
+    model : pyomo.environ.ConcreteModel or None
+        Model
+    results : return of pyomo.opt.SolverFactory or None
+        Results of solve
+    """
+    def __init__(
+        self,
+        periods,
+        D,
+        C,
+        S,
+        p,
+        r,
+        R, 
+        vc=0.0, 
+        vd=0.0,
+        solver='glpk'
+    ):
+        """
+        Parameters
+        __________
+        periods : int
+            Number of periods to run model for, must match length of period indexed arrays
+        D : numpy.array
+            Maximum discharge rate indexed by period as 1-D numpy.array
+        C : numpy.array
+            Maximum charge rate indexed by period as 1-D numpy.array
+        S : numpy.array
+            Storage capacity indexed by period as 1-D numpy.array
+        p : numpy.array
+            Energy price indexed by period as 1-D numpy.array 
+        r : float
+            Discount rate
+        R : numpy.array or float
+            Round trip efficiency matrix time dependent on charge (i), discharge (j) periods or non time dependent
+        vc : float, default 0.0
+            Variable charge cost
+        vd : float, default 0.0
+            Variable discharge cost
+        solver : str, default 'glpk'
+            Solver to use in SolverFactory
+        """
+
+        self.periods = periods
+        self.D = D
+        self.C = C
+        self.S = S
+        self.p = p
+        self.r = r
+        self.R = R
+        self.vc = vc
+        self.vd = vd
+        self.solver = solver
+
+        self.model = None
+        self.results = None
+    
+    def setup_model(
+        self
+    ):
+        """
+        Setup model
+        """
+
+        raise NotImplementedError
+
+    def solve_model(
+        self
+    ):
+        """
+        Solve model
+        """
+
+        if self.model is None: 
+            raise ValueError('Please setup model before solving')
+        
+        start = time.time()
+
+        solver_factory = SolverFactory(self.solver)
+        self.results = solver_factory.solve(self.model)
+        print(f"""
+        Solution Results for model
+            STATUS: {self.results['Solver'][0]['Status'].value.upper()}
+            TERMINATION CONDITION: {self.results['Solver'][0]['Termination condition'].value.upper()}
+            OBJECTIVE LOWER BOUND: {self.results['Problem'][0]['Lower bound']}
+            OBJECTIVE UPPER BOUND: {self.results['Problem'][0]['Lower bound']}
+        """)
+        self.model.solutions.store_to(self.results)  # store solutions too
+
+        elapsed_time_display(start, ' (model solve time)')
+
+    def write_to_json(
+        self, 
+        filename
+    ):
+        """
+        Write results to a JSON
+
+        Parameters
+        __________
+        filename : str
+            Filename and location to save JSON, must end in '.json'
+        """
+
+        if self.results is None: 
+            raise ValueError('Please use solve_model method before writing results as JSON')
+
+        self.results.write(filename=filename, format='json')
+
+
+class proposal_v2_1(model):
+    """
+    Updated model detailed in theory in Team Project Proposal Follow-up v2.docx
+
+    Attributes
+    __________
+    valid_pair_span : int
+        Valid maximum span between pairs of periods measured in periods
+    periods : int
+        Number of periods to run model for, must match length of period indexed arrays
+    D : numpy.array
+        Maximum discharge rate indexed by period as 1-D numpy.array
+    C : numpy.array
+        Maximum charge rate indexed by period as 1-D numpy.array
+    S : numpy.array
+        Storage capacity indexed by period as 1-D numpy.array
+    p : numpy.array
+        Energy price indexed by period as 1-D numpy.array 
+    r : float
+        Discount rate
+    R : numpy.array or float
+        Round trip efficiency matrix time dependent on charge (i), discharge (j) periods or non time dependent
+    vc : float
+        Variable charge cost
+    vd : float
+        Variable discharge cost
+    solver : str
+        Solver to use in SolverFactory
+    model : pyomo.environ.ConcreteModel or None
+        Model
+    results : return of pyomo.opt.SolverFactory or None
+        Results of solve
+    """
+    def __init__(
+        self,
+        valid_pair_span,
+        periods,
+        D,
+        C,
+        S,
+        p,
+        r,
+        R, 
+        vc=0.0, 
+        vd=0.0,
+        solver='glpk'
+    ):
+        """
+        Parameters
+        __________
+        valid_pair_span : int
+            Valid maximum span between pairs of periods measured in periods
+        periods : int
+            Number of periods to run model for, must match length of period indexed arrays
+        D : numpy.array
+            Maximum discharge rate indexed by period as 1-D numpy.array
+        C : numpy.array
+            Maximum charge rate indexed by period as 1-D numpy.array
+        S : numpy.array
+            Storage capacity indexed by period as 1-D numpy.array
+        p : numpy.array
+            Energy price indexed by period as 1-D numpy.array 
+        r : float
+            Discount rate
+        R : numpy.array or float
+            Round trip efficiency matrix time dependent on charge (i), discharge (j) periods or non time dependent
+        vc : float, default 0.0
+            Variable charge cost
+        vd : float, default 0.0
+            Variable discharge cost
+        solver : str, default 'glpk'
+            Solver to use in SolverFactory
+        """
+
+        if isinstance(R, float):
+            R = np.full((periods, periods), R)  # will only reference valid periods
+        super().__init__(periods, D, C, S, p, r, R, vc, vd, solver)
+
+        self.valid_pair_span = valid_pair_span
+
+    def setup_model(
+        self
+    ):
+        """
+        Setup model
+        """
+
+        start = time.time()
+
+        T = np.arange(0, self.periods)  # use native 0 indexing in Python
+        V = [(_, _ + span) for _ in T for span in np.arange(1, self.valid_pair_span + 1) if _ + span < self.periods]  # only consider valid periods
+        
+        self.model = pyo.ConcreteModel()
+        self.model.constraints = pyo.ConstraintList()
+        self.model.f = pyo.Var(V, domain=pyo.NonNegativeReals)
+        self.model.s = pyo.Var(T, domain=pyo.NonNegativeReals)
+
+        self.model.objective = pyo.Objective(
+            expr=sum(
+                (self.model.f[idx] * ((self.p[idx[1]] - self.vd) * self.R[idx[0]][idx[1]] - (self.p[idx[0]] + self.vc))) *  # pair arb
+                (np.exp(-self.r * idx[1] / self.periods)) for idx in V),  # discounting  
+            sense=pyo.maximize
+        )
+
+        for pd in T:  # TODO: can for loop be vectorized, otherwise streamlined
+            self.model.constraints.add(self.model.s[pd] <= self.S[pd])
+            self.model.constraints.add(self.model.s[pd] == sum(self.model.f[idx] for idx in V if ((idx[0] <= pd) & (idx[1] > pd))))
+            try: 
+                self.model.constraints.add(sum(self.model.f[idx] for idx in V if (idx[1] == pd)) <= self.D[pd])
+            except ValueError:  # no valid periods
+                pass
+            try: 
+                self.model.constraints.add(sum(self.model.f[idx] for idx in V if (idx[0] == pd)) <= self.C[pd])    
+            except ValueError:  # no valid periods
+                pass
+
+        elapsed_time_display(start, ' (model setup time)')
+
+
+class linear_model_v1_0(model):
+    """
+    Efficient model detailed by Minh with separated charge, discharge periods
+
+    Attributes
+    __________
+    max_charge : int
+        Maximum number of batch charges allowed
+    batch_runtime : int
+        Number of periods it takes to charge (no discharge allowed)
+    periods : int
+        Number of periods to run model for, must match length of period indexed arrays
+    D : numpy.array
+        Maximum discharge rate indexed by period as 1-D numpy.array
+    C : numpy.array
+        Maximum charge rate indexed by period as 1-D numpy.array
+    S : numpy.array
+        Storage capacity indexed by period as 1-D numpy.array
+    p : numpy.array
+        Energy price indexed by period as 1-D numpy.array 
+    r : float
+        Discount rate
+    R : numpy.array or float
+        Round trip efficiency matrix time dependent on charge (i), discharge (j) periods or non time dependent
+    vc : float
+        Variable charge cost
+    vd : float
+        Variable discharge cost
+    solver : str
+        Solver to use in SolverFactory
+    model : pyomo.environ.ConcreteModel or None
+        Model
+    results : return of pyomo.opt.SolverFactory or None
+        Results of solve
+    """
+    def __init__(
+        self,
+        max_charge, 
+        batch_runtime, 
+        periods,
+        D,
+        C,
+        S,
+        p,
+        r,
+        R, 
+        vc=0.0, 
+        vd=0.0,
+        solver='glpk'
+    ):
+        """
+        Parameters
+        __________
+        max_charge : int
+            Maximum number of batch charges allowed
+        batch_runtime : int
+            Number of periods it takes to charge (no discharge allowed)
+        periods : int
+            Number of periods to run model for, must match length of period indexed arrays
+        D : numpy.array
+            Maximum discharge rate indexed by period as 1-D numpy.array
+        C : numpy.array
+            Maximum charge rate indexed by period as 1-D numpy.array
+        S : numpy.array
+            Storage capacity indexed by period as 1-D numpy.array
+        p : numpy.array
+            Energy price indexed by period as 1-D numpy.array 
+        r : float
+            Discount rate
+        R : numpy.array or float
+            Round trip efficiency matrix time dependent on charge (i), discharge (j) periods or non time dependent
+        vc : float, default 0.0
+            Variable charge cost
+        vd : float, default 0.0
+            Variable discharge cost
+        solver : str, default 'glpk'
+            Solver to use in SolverFactory
+        """
+
+        if isinstance(R, np.ndarray):
+            raise ValueError('This model does not accept matrix or numpy.array R input')
+        super().__init__(periods, D, C, S, p, r, R, vc, vd, solver)
+
+        self.max_charge = max_charge
+        self.batch_runtime = batch_runtime
+
+    def setup_model(
+        self
+    ):
+        """
+        Setup model
+        """
+
+        start = time.time()
+
+        T = np.arange(0, self.periods)  # use native 0 indexing in Python
+        big_M = float('inf')
+        
+        self.model = pyo.ConcreteModel()
+        self.model.constraints = pyo.ConstraintList()
+        self.model.d = pyo.Var(T, domain=pyo.NonNegativeReals) # Discharge
+        self.model.c = pyo.Var(T, domain=pyo.NonNegativeReals) # Charge
+        self.model.s = pyo.Var(T, domain=pyo.NonNegativeReals) # Current storage
+        self.model.charging = pyo.Var(T, domain=pyo.Binary) # Binary variable indicating whether batch charging is in progress
+
+        self.model.objective = pyo.Objective(
+            expr=sum(
+                ((self.p[t] - self.vd) * self.R * self.model.d[t] - (self.p[t] + self.vc) * self.model.c[t]) *   # pair arb
+                (np.exp(-self.r * t / self.periods)) for t in T),  # discounting   
+            sense=pyo.maximize
+        )
+
+        self.model.constraints.add(self.model.s[0] == 0) # initial storage set to 0
+        self.model.constraints.add(sum(self.model.charging[t] for t in T) <= (self.max_charge * self.batch_runtime))
+        for pd in T:  # TODO: can for loop be vectorized, otherwise streamlined
+            self.model.constraints.add(self.model.s[pd] <= self.S[pd])
+            self.model.constraints.add(self.model.d[pd] <= self.D[pd])
+            self.model.constraints.add(self.model.d[pd] <= self.model.s[pd])  # discharge cannot exceed what's currently in storage
+            self.model.constraints.add(self.model.c[pd] <= self.C[pd] * self.model.charging[pd])  # if not charging then c[t] is zero
+
+            if pd > 0:
+                self.model.constraints.add(self.model.s[pd-1] + self.model.c[pd] - self.model.d[pd] == self.model.s[pd])  # storage equation
+
+                # The following constraints ensure that batch charging lasts exactly batch_runtime
+                # Refer to: https://yetanothermathprogrammingconsultant.blogspot.com/2018/03/production-scheduling-minimum-up-time.html
+                self.model.constraints.add(sum([self.model.charging[k] for k in range(pd, min(self.periods, pd + self.batch_runtime))]) >= self.batch_runtime * (self.model.charging[pd] - self.model.charging[pd - 1]))   
+                self.model.constraints.add(sum([self.model.charging[k] for k in range(pd, min(self.periods, pd + self.batch_runtime + 1))]) <= self.batch_runtime)
+            else:
+                self.model.constraints.add(sum([self.model.charging[k] for k in range(0, min(self.periods, self.batch_runtime))]) >= self.batch_runtime * (self.model.charging[0]))  
+            
+            self.model.constraints.add(self.model.d[pd] <= big_M * (1 - self.model.charging[pd])) # cannot discharge while charging
+
+        elapsed_time_display(start, ' (model setup time)')
--- run_model_template.py
+++ run_model_template.py
@@ -1,11 +1,58 @@
 """
-Run instantiated models from model classes using average ERCOT prices for 2019-2022
+Run instantiated model from model classes using average ERCOT prices for 2019-2022
 """
 
 import numpy as np
 import pandas as pd
 
+from models import proposal_v2_1, linear_model_v1_0
+
 
 ercot_data_folder = f"{__file__.split('ORI_390Q8_Team_Project')[0]}/ORI_390Q8_Team_Project/data/ERCOT/"
 dam_avg = pd.read_csv(f"{ercot_data_folder}/post_processing/dam_avg.csv")
-rtm_avg = pd.read_csv(f"{ercot_data_folder}/post_processing/rtm_avg.csv")
+# rtm_avg = pd.read_csv(f"{ercot_data_folder}/post_processing/rtm_avg.csv")  # TODO: fix this dataset, use
+
+mod1_hours = 8760
+mod1_theta_t = np.linspace(1, 0.8, mod1_hours * 10)  # decline to 80% after ten years
+mod1_theta_t = np.concatenate([mod1_theta_t, np.linspace(mod1_theta_t[-1], 0.1, mod1_hours * 10)])  # decline to 10% after twenty years, should be non-linear, concave
+mod1_theta_t = np.concatenate([mod1_theta_t, np.linspace(mod1_theta_t[-1], 0.0, mod1_hours * 10)])  # decline to 0% after thirty years, should be non-linear, convex
+mod1 = proposal_v2_1(
+    valid_pair_span=24,
+    periods=mod1_hours,
+    D=np.full(mod1_hours, 0.9 * 100),
+    C=np.full(mod1_hours, 0.95 * 100),
+    S=(100 * mod1_theta_t)[:mod1_hours],
+    p=dam_avg.loc[dam_avg['SETTLEMENT_POINT'] == 'LZ_LCRA'].sort_values(['MONTH', 'DAY', 'HOUR_ENDING'])['PRICE'].to_numpy()[:mod1_hours],
+    r=0.05,  # 5 to 10% noted to be a good assumption by Dr. Leibowicz
+    R=0.915, 
+    vc=0.0, 
+    vd=0.0,
+    solver='glpk'
+)
+mod1.setup_model()
+mod1.solve_model()
+mod1.write_to_json(filename='mod1.json')
+
+# mod2_hours = 8760
+# mod2_theta_t = np.linspace(1, 0.8, mod2_hours * 10)  # decline to 80% after ten years
+# mod2_theta_t = np.concatenate([mod2_theta_t, np.linspace(mod2_theta_t[-1], 0.1, mod2_hours * 10)])  # decline to 10% after twenty years, should be non-linear, concave
+# mod2_theta_t = np.concatenate([mod2_theta_t, np.linspace(mod2_theta_t[-1], 0.0, mod2_hours * 10)])  # decline to 0% after thirty years, should be non-linear, convex
+# mod2 = linear_model_v1_0(
+#     max_charge=2,
+#     batch_runtime=2, 
+#     periods=8760,
+#     D=np.full(8760, 0.9 * 100),
+#     C=np.full(8760, 0.95 * 100),
+#     S=(100 * mod2_theta_t)[:8760],
+#     p=dam_avg.loc[dam_avg['SETTLEMENT_POINT'] == 'LZ_LCRA'].sort_values(['MONTH', 'DAY', 'HOUR_ENDING'])['PRICE'].to_numpy(),
+#     r=0.05,  # 5 to 10% noted to be a good assumption by Dr. Leibowicz
+#     R=0.915, 
+#     vc=0.0, 
+#     vd=0.0,
+#     solver='glpk'
+# )
+# mod2.setup_model()
+# mod2.solve_model()
+# mod2.write_to_json(filename='mod2.json')
+
+# Test for daily, weekly granularity tech
