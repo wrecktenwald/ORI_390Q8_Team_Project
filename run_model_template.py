247e40f6b51e766b264b96e11c4e2f758e1581c7
Minh Vu <mdvu15@earlham.edu>
2023-04-16 17:27:34 -0500

    Adjust max_charge to test mod2, mod2 works with gurobi

--- run_model_template.py
+++ run_model_template.py
@@ -33,26 +33,26 @@ mod1.setup_model()
 mod1.solve_model()
 mod1.write_to_json(filename='mod1.json')
 
-# mod2_hours = 8760
-# mod2_theta_t = np.linspace(1, 0.8, mod2_hours * 10)  # decline to 80% after ten years
-# mod2_theta_t = np.concatenate([mod2_theta_t, np.linspace(mod2_theta_t[-1], 0.1, mod2_hours * 10)])  # decline to 10% after twenty years, should be non-linear, concave
-# mod2_theta_t = np.concatenate([mod2_theta_t, np.linspace(mod2_theta_t[-1], 0.0, mod2_hours * 10)])  # decline to 0% after thirty years, should be non-linear, convex
-# mod2 = linear_model_v1_0(
-#     max_charge=2,
-#     batch_runtime=2, 
-#     periods=8760,
-#     D=np.full(8760, 0.9 * 100),
-#     C=np.full(8760, 0.95 * 100),
-#     S=(100 * mod2_theta_t)[:8760],
-#     p=dam_avg.loc[dam_avg['SETTLEMENT_POINT'] == 'LZ_LCRA'].sort_values(['MONTH', 'DAY', 'HOUR_ENDING'])['PRICE'].to_numpy(),
-#     r=0.05,  # 5 to 10% noted to be a good assumption by Dr. Leibowicz
-#     R=0.915, 
-#     vc=0.0, 
-#     vd=0.0,
-#     solver='glpk'
-# )
-# mod2.setup_model()
-# mod2.solve_model()
-# mod2.write_to_json(filename='mod2.json')
+mod2_hours = 8760
+mod2_theta_t = np.linspace(1, 0.8, mod2_hours * 10)  # decline to 80% after ten years
+mod2_theta_t = np.concatenate([mod2_theta_t, np.linspace(mod2_theta_t[-1], 0.1, mod2_hours * 10)])  # decline to 10% after twenty years, should be non-linear, concave
+mod2_theta_t = np.concatenate([mod2_theta_t, np.linspace(mod2_theta_t[-1], 0.0, mod2_hours * 10)])  # decline to 0% after thirty years, should be non-linear, convex
+mod2 = linear_model_v1_0(
+    max_charge=2,
+    batch_runtime=2, 
+    periods=8760,
+    D=np.full(8760, 0.9 * 100),
+    C=np.full(8760, 0.95 * 100),
+    S=(100 * mod2_theta_t)[:8760],
+    p=dam_avg.loc[dam_avg['SETTLEMENT_POINT'] == 'LZ_LCRA'].sort_values(['MONTH', 'DAY', 'HOUR_ENDING'])['PRICE'].to_numpy(),
+    r=0.05,  # 5 to 10% noted to be a good assumption by Dr. Leibowicz
+    R=0.915, 
+    vc=0.0, 
+    vd=0.0,
+    solver='gurobi'
+)
+mod2.setup_model()
+mod2.solve_model()
+mod2.write_to_json(filename='mod2.json')
 
 # Test for daily, weekly granularity tech
