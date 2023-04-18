05341c8c332f51bd6ee01bf63ebd65f5d5f41711
wrecktenwald <wrecktenwald@utexas.edu>
2023-04-17 22:10:47 -0500

    Add read_model_results_json utility to retrieve results from JSON files

--- utilities.py
+++ utilities.py
@@ -3,6 +3,8 @@ Utilities
 """
 
 import time
+import json
+import pandas as pd
 
 
 def elapsed_time_display(
@@ -28,3 +30,45 @@ def elapsed_time_display(
     elapsed %= 60
 
     print(f"Elapsed time{insert_phrase}: %02d:%02d:%02d" % (hour, min, sec)) 
+
+
+def read_model_results_json(
+    filename
+):
+    """
+    Read critical results data in JSON written to JSON from a pyomo model 
+
+    Parameters
+    __________
+    filename : str
+        Filename and location to read JSON, must end in '.json'
+    """
+
+    f = open(filename)
+    _json_ = json.load(f)
+    f.close()
+
+    if len(_json_['Solution']) > 2:
+        raise ValueError("Unexpected length of 'Solution' (greater than 2)")
+
+    results =  {
+        'Lower bound': _json_['Problem'][0]['Lower bound'],
+        'Upper bound': _json_['Problem'][0]['Upper bound'],
+        'Optimality Gap': _json_['Solution'][1]['Gap'],
+    }
+
+    try:
+        results['Objective'] = _json_['Solution'][1]['Objective']['objective']['Value']
+    except KeyError:
+        try:
+            results['Objective'] = _json_['Solution'][1]['Objective']['OBJ']['Value']
+        except KeyError:
+            print(f"Check returned 'Objective' for {filename}")
+            results['Objective'] = _json_['Solution'][1]['Objective']
+
+    results.update({
+        'Status': _json_['Solution'][1]['Status'], 
+        'Decision Variables': pd.Series({k: v['Value'] for k, v in _json_['Solution'][1]['Variable'].items()})
+    })
+
+    return results
